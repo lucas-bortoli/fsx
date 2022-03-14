4819136a26a01851549f848cc94770f64080a57a
Lucas Bortoli <lucasbortolisantos@gmail.com>
2022-03-14 09:14:08 -0300

    File upload/download

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { isAbsolute, resolve } from 'path'
+import FileSystem from '@lucas-bortoli/libdiscord-fs'
+import { FileHandle } from 'fs/promises'
+import { EventEmitter } from 'stream'
 
 const REMOTE_PATH_REGEXP = /^([A-Za-zÀ-ÖØ-öø-ÿ]+)::(\/.*)$/
 
-interface ParsedRemotePath { driveId: string, path: string }
+interface ParsedRemotePath { driveId: string, remotePath: string }
+
+// All log messages should be written to stderr, because stdout is used for file
+// data
+console.log = console.error
 
 /**
  * Checks if a given path is remote. Remote paths start with "driveName::/"
@@ -13,7 +20,46 @@ const isValidRemotePath = (p: string): boolean => {
 
 const parseRemotePath = (p: string): ParsedRemotePath => {
     const match = p.match(REMOTE_PATH_REGEXP)
-    return { driveId: match[1], path: match[2] }
+    return { driveId: match[1], remotePath: match[2] }
+}
+
+const waitForEvent = (emitter: EventEmitter, event: string): Promise<void> => {
+    return new Promise(resolve => {
+        emitter.once(event, () => resolve())
+    })
+}
+
+/**
+ * Initializes a filesystem object.
+ * @param driveId What drive id to use for the filesystem
+ */
+const openFileSystem = async (driveId: string): Promise<FileSystem> => {
+    const fs = new FileSystem(`${driveId}.fsx`, process.env.DISCORD_WEBHOOK)
+    await fs.loadDataFile()
+    return fs
+}
+
+const showHelpPageAndExit = (errorToBeShown?: string): never => {
+    console.error(
+`fsx Help Page
+
+Usage:
+
+    $ fsx {download|upload|ls|rm|mv} [...Arguments]
+
+Available commands:
+        download driveId::/path/to/remote/file
+        The downloaded data is piped to STDOUT. Use shell redirection to write it to disk.
+        EXAMPLE:
+            $ fsx download drive::/documents/file.txt > file.txt
+
+        upload driveId::/path/to/where/file/will/be/stored
+        The file stream is read from STDIN. Pipe a file using your shell to upload it.
+        EXAMPLE:
+            $ cat file.txt | fsx upload drive::/documents/file.txt
+${errorToBeShown ? `${'-'.repeat(errorToBeShown.length + 7)}\nError: ${errorToBeShown}` : ''}`
+    )
+    return process.exit(1)
 }
 
 const main = async () => {
@@ -32,31 +78,45 @@ const main = async () => {
     const operand1 = args[1]
     const operand2 = args[2]
 
+    if (operation === 'download') {
+        if (!operand1)
+            return showHelpPageAndExit('download: Missing parameters')
 
-    const op1 = resolve(process.cwd(), process.argv[2])
-    const op2 = process.argv[3]
+        if (!isValidRemotePath(operand1))
+            return showHelpPageAndExit(`download: Invalid remote path ${operand1} (not in format driveId::/path/to/file)`)
 
-    console.log(`op1: ${op1} isAbsolute: ${isAbsolute(op1)}`)
-    console.log(`op2: ${op2} isAbsolute: ${isAbsolute(op2)}`)
-}
+        const { driveId, remotePath } = parseRemotePath(operand1)
+        const fileSystem = await openFileSystem(driveId)
 
+        if (!await fileSystem.getFileEntry(remotePath)) {
+            console.error(`File doesn't exist: ${driveId}::${remotePath}`)
+            return process.exit(1)
+        }
 
-const showHelpPageAndExit = (errorToBeShown?: string): never => {
-    console.error(
-`fsx Help Page
+        const remoteFileStream = await fileSystem.createReadStream(remotePath)
 
-Usage:
+        remoteFileStream.pipe(process.stdout)
 
-    $ fsx <Command: download|upload|ls|rm|mv> [...Arguments]
+        await waitForEvent(remoteFileStream, 'finish')
+    } else if (operation === 'upload') {
+        if (!operand1)
+            return showHelpPageAndExit('upload: Missing parameters')
 
-Available commands:
-        download driveId::/path/to/remote/file ./path/to/downloaded/file
+        if (!isValidRemotePath(operand1))
+            return showHelpPageAndExit(`upload: Invalid remote path ${operand1} (not in format driveId::/path/to/file)`)
 
-        upload ./path/to/local/file driveId::/path/to/where/file/will/be/stored
+        const { driveId, remotePath } = parseRemotePath(operand1)
+        const fileSystem = await openFileSystem(driveId)
+        const remoteFileStream = await fileSystem.createWriteStream(remotePath)
 
-${errorToBeShown ? `${'-'.repeat(errorToBeShown.length + 7)}\nError: ${errorToBeShown}` : ''}`
-    )
-    return process.exit(1)
+        process.stdin.pipe(remoteFileStream)
+
+        await waitForEvent(remoteFileStream, 'finish')
+        await fileSystem.writeDataFile()
+        console.log('File uploaded')
+    } else {
+        return showHelpPageAndExit(`Invalid command: ${operation}`)
+    }
 }
 
 main()
